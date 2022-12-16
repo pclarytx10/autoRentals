1d15061841959e20064d5bd0fd4f873f0731c5c4
Patrick Clary <patrickm@theclarys.org>
2022-12-16 09:23:23 -0600

    Add middleware, refactor server.js

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
+// Dependencies
 const express = require("express")
 const app = express()
 app.use(express.static('public'));
 const cars = require("./models/cars.js") 
+
 // Environment Variables
 require("dotenv").config()
 
+// Middleware
+app.use(express.urlencoded({extended: true}))
+
+// Routes
+// Root Route
 app.get('/', (req,res) => {
     res.send('Welcome to dPremium Auto Rentals!')
 })
 
-app.get('/cars/:id', (req,res) => {
-    res.render('show.ejs', {
-        car: cars[req.params.id],
-    })
-})
-
+// Index Route
 app.get('/cars', (req,res) => {
     res.render('index.ejs', {
         allCars: cars
+    })
+})
+
+// New Route
+
+// Delete Route
+
+// Update Route
+
+// Create Route
+
+// Edit Route
+
+// Show Route  
+app.get('/cars/:id', (req,res) => {
+    res.render('show.ejs', {
+        car: cars[req.params.id],
     })
 })
 
