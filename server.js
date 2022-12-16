6a018f69e6111292710991c0ca4b8de35de35b7a
Patrick Clary <patrickm@theclarys.org>
2022-12-16 08:33:11 -0600

    Add seed and dotenv files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const app = express()
 app.use(express.static('public'));
-const PORT = 3000
-
 const cars = require("./models/cars.js") 
+// Environment Variables
+require("dotenv").config()
 
 app.get('/', (req,res) => {
     res.send('Welcome to dPremium Auto Rentals!')
@@ -21,6 +21,8 @@ app.get('/cars', (req,res) => {
     })
 })
 
+// Listen
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log("Fast cars, slow cars...")
 })
