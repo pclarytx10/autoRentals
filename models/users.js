f490f956020e86533844b7d0b2d6db7fd9fb092e
Patrick Clary <patrickm@theclarys.org>
2022-12-22 22:18:22 -0600

    Update user model with street, city, state, zip

--- models/users.js
+++ models/users.js
@@ -8,11 +8,15 @@ const UserSchema = new Schema({
     password: {type: String, required: [true, "Password is required."]},
     firstName: {type: String, required: [true, "First name is required."]},
     lastName: {type: String, required: [true, "Last name is required."]},
-    address: {type: String, required: [true, "Address is required."]}
-});
+    addressStreet: {type: String, required: [true, "Address is required."]},
+    addressCity: {type: String, required: [true, "City is required."]},
+    addressState: {type: String, required: [true, "State is required."]},
+    addressZip: {type: String, required: [true, "Zip code is required."]},
+}, {timestamps: true});
 
 // Create Model
 const Users = mongoose.model('User', UserSchema);
 
 // Export Model
 module.exports = Users;
+
