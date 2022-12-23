bf11995b567c5c3a0f55b56ab4893ce857589f18
Patrick Clary <patrickm@theclarys.org>
2022-12-22 21:51:31 -0600

    Add Users New, Edit, Index, & Show page templates

---
+++ Controllers/users.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require('express');
+const userRouter = express.Router();
+const Users = require('../models/users.js');
+const bcrypt = require('bcrypt');
+
+// Index Route
+
+// New Route
+
+// Delete Route
+
+// Update Route
+
+// Create Route
+
+// Edit Route
+
+// Show Route
+
+
+// Export Router
+module.exports = userRouter;
