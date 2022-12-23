5ead15d420e4b73dc7f270908c61a64b38c29326
Patrick Clary <patrickm@theclarys.org>
2022-12-23 09:29:56 -0600

    Add currentUser to rentals.js controller

--- Controllers/rentals.js
+++ Controllers/rentals.js
@@ -9,7 +9,9 @@ const rentalsSeed = require('../models/rentalSeed.js')
 rentalRouter.get('/seed', (req, res) => {
     Rentals.deleteMany({}, (error, allRentals) => {})
     Rentals.create(rentalsSeed, (error, data) => {
-        res.redirect('/rentals')
+        res.redirect('/rentals', {
+            currentUser: req.session.currentUser
+        })
     })
 })
 
@@ -17,20 +19,25 @@ rentalRouter.get('/seed', (req, res) => {
 rentalRouter.get('/', (req,res) => {
     Rentals.find({}, (err, allRentals) => {     
     res.render('rentals/index.ejs', {
-        rentals: allRentals
+        rentals: allRentals,
+        currentUser: req.session.currentUser
         })
     })
 })
 
 // New Route
 rentalRouter.get('/new', (req,res) => {
-    res.render('rentals/new.ejs')
+    res.render('rentals/new.ejs', {
+        currentUser: req.session.currentUser
+    })
 })
 
 // Delete Route
 rentalRouter.delete('/:id', (req,res) => {
     Rentals.findByIdAndRemove(req.params.id, (err, data) => {
-        res.redirect('/rentals')
+        res.redirect('/rentals', {
+            currentUser: req.session.currentUser
+        })
     })
 })
 
@@ -39,14 +46,18 @@ rentalRouter.put('/:id', (req,res) => {
     Rentals.findByIdAndUpdate(req.params.id, 
         req.body , {new:true}, 
         (err, updatedModel) => {
-            res.redirect('/rentals')
+            res.redirect('/rentals', {
+                currentUser: req.session.currentUser
+            })
         })
 })
 
 // Create Route
 rentalRouter.post('/', (req,res) => {
     Rentals.create(req.body, (error, createdRental) => {
-        res.redirect('/rentals')
+        res.redirect('/rentals', {
+            currentUser: req.session.currentUser
+        })
     })
 })
 
@@ -54,7 +65,8 @@ rentalRouter.post('/', (req,res) => {
 rentalRouter.get('/:id/edit', (req,res) => {
     Rentals.findById(req.params.id, (err, foundRental) => { 
         res.render('rentals/edit.ejs', {
-            rental: foundRental
+            rental: foundRental,
+            currentUser: req.session.currentUser
         })
     })
 })
@@ -63,7 +75,8 @@ rentalRouter.get('/:id/edit', (req,res) => {
 rentalRouter.get('/:id', (req,res) => {
     Rentals.findById(req.params.id, (err, foundRental) => {
         res.render('rentals/show.ejs', {
-            rental: foundRental
+            rental: foundRental,
+            currentUser: req.session.currentUser
         })
     })
 })
