ee5642e4101f219e08214617c054fdb2169d33eb
Patrick Clary <patrickm@theclarys.org>
2023-01-02 21:44:44 -0600

    Remove localStorage calls

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -36,7 +36,6 @@ sessionsRouter.post('/', (req,res) => {
 
 // Show Route
 sessionsRouter.get('/logout', (req,res) => {
-    console.log(localStorage.getItem('dpremiumUser'))
         res.render('sessions/logout.ejs', {
         currentUser: req.session.currentUser
     })
